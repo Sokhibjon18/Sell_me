e8a350e9b4282886319d94c6c7a2c2d6dd9a47ac
Sokhibjon Safarov <safarovsoxibjon18@gmail.com>
2020-12-22 01:32:04 +0500

    Some changes

--- app/src/main/java/uz/triples/sellme/main/MainScreen.kt
+++ app/src/main/java/uz/triples/sellme/main/MainScreen.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_main.*
 import uz.triples.sellme.R
 import uz.triples.sellme.data.CategoryData
@@ -14,55 +15,56 @@ import uz.triples.sellme.data.ProductData
 import uz.triples.sellme.main.adapter.CategoryAdapter
 import uz.triples.sellme.main.adapter.LocationProductAdapter
 import uz.triples.sellme.main.adapter.ProductAdapter
-import uz.triples.sellme.utils.hideKeyboard
 
 class MainScreen : Fragment(R.layout.fragment_main) {
-    val adapter by lazy { CategoryAdapter() }
-    val pr_adapter by lazy { ProductAdapter() }
-    val loc_pr_adapter by lazy { LocationProductAdapter() }
-    val categorydata = ArrayList<CategoryData>()
-    val productdata = ArrayList<ProductData>()
-    val locationproductdata = ArrayList<ProductData>()
+    private val adapter by lazy { CategoryAdapter() }
+    private val prAdapter by lazy { ProductAdapter() }
+    private val locPrAdapter by lazy { LocationProductAdapter() }
+    private val categoryData = ArrayList<CategoryData>()
+    private val productData = ArrayList<ProductData>()
+    private val locationProductData = ArrayList<ProductData>()
+    private val mAuth = FirebaseAuth.getInstance()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        hideKeyboard()
-
         loadCategoryData()
-        adapter.submitList(categorydata)
+        adapter.submitList(categoryData)
         list.adapter = adapter
         list.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
         loadProductData()
-        pr_adapter.submitList(productdata)
-        list_product1.adapter = pr_adapter
+        prAdapter.submitList(productData)
+        list_product1.adapter = prAdapter
         list_product1.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
         loadLocationProductData()
-        loc_pr_adapter.submitList(locationproductdata)
-        location_product_list.adapter = loc_pr_adapter
+        locPrAdapter.submitList(locationProductData)
+        location_product_list.adapter = locPrAdapter
         location_product_list.layoutManager =
             GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
 
-        loc_pr_adapter.setOnClickListener {
+        locPrAdapter.setOnClickListener {
             Toast.makeText(context, "item position = $it", Toast.LENGTH_SHORT).show()
         }
 
 
         upload_image.setOnClickListener {
-            findNavController().navigate(R.id.action_mainScreen_to_cameraScreen)
+            if (mAuth.currentUser == null)
+                findNavController().navigate(R.id.loginFragment)
+            else
+                findNavController().navigate(R.id.cameraScreen)
         }
 
 
-        pr_adapter.setOnClickFavoriteListener {
-            if (productdata[it].isfavorite) {
-                productdata[it].isfavorite = false
-                pr_adapter.notifyDataSetChanged()
+        prAdapter.setOnClickFavoriteListener {
+            if (productData[it].isfavorite) {
+                productData[it].isfavorite = false
+                prAdapter.notifyDataSetChanged()
             } else {
-                productdata[it].isfavorite = true
-                pr_adapter.notifyDataSetChanged()
+                productData[it].isfavorite = true
+                prAdapter.notifyDataSetChanged()
             }
         }
 
@@ -70,19 +72,19 @@ class MainScreen : Fragment(R.layout.fragment_main) {
 
     fun loadCategoryData() {
         for (i in 0..20) {
-            categorydata.add(CategoryData(i, "name $i", R.drawable.car))
+            categoryData.add(CategoryData(i, "name $i", R.drawable.car))
         }
     }
 
     fun loadProductData() {
         for (i in 0..20) {
-            productdata.add(ProductData(i, R.drawable.productimg, true))
+            productData.add(ProductData(i, R.drawable.productimg, true))
         }
     }
 
     fun loadLocationProductData() {
         for (i in 0..100) {
-            locationproductdata.add(ProductData(i, R.drawable.productimg, true))
+            locationProductData.add(ProductData(i, R.drawable.productimg, true))
         }
     }
 }
